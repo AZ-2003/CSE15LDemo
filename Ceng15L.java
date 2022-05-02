b3bba974fe208af708723a1ca31bb628c00e4af9
AZ-2003 <ali.z03@yahoo.com>
2022-05-01 18:49:59 -0700

    Fix

--- Ceng15L.java
+++ Ceng15L.java
@@ -1,3 +1,11 @@
 public class Ceng15L
 {
+    public int Divide(int dividend, int divisor)
+    {
+        if(divisor == 0)
+        {
+            return 0;
+        }
+        return dividend/divisor;
+    }
 }
--- Ceng15LTester.java
+++ Ceng15LTester.java
@@ -1,3 +1,31 @@
+import static org.junit.Assert.*;
+import org.junit.*;
+import java.util.*;
+import java.io.IOException;
+
 public class Ceng15LTester
 {
+    Ceng15L test;
+    @Before
+    public void setup()
+    {
+        test = new Ceng15L();
+    }
+    @Test
+    public void Test() 
+    {
+        boolean exception = false;
+        try 
+        {
+            test.Divide(5,0);
+        }
+        catch(ArithmeticException e)
+        {
+            exception = true;
+        }
+        assertFalse("exception thrown", exception);
+        assertEquals(0, test.Divide(5,0));
+    }
+    
+    
 }
